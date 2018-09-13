71a3fd2042a669eb4ea76c96e9efbafdf3f4ec05
Grant Elliott <gelliott@acumensolutions.com>
2018-09-13 10:08:50 -0400

    First pass at a programmatic table view.

--- Programmatic Table View Example/ViewController.swift
+++ Programmatic Table View Example/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var tableView: UITableView!
+    let data = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView = UITableView(frame: self.view.bounds, style: .plain)
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.view.addSubview(self.tableView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let datum: String = self.data[indexPath.row]
+        
+        cell.textLabel!.text = datum
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(self.data[indexPath.row])
+    }
 }
 
